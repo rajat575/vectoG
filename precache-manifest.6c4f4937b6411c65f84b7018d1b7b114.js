4b1d7b5130ff04991b49c4bc5abd12297ea77746
rajat575 <sharmarajat575@gmail.com>
2022-06-01 18:27:48 +0530

    first

---
+++ precache-manifest.6c4f4937b6411c65f84b7018d1b7b114.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595af77d8d39367c08b27751ffbf0091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72724b06355cea6e311b",
+    "url": "/static/css/2.ca69a582.chunk.css"
+  },
+  {
+    "revision": "78c812a429ee0b399dd8",
+    "url": "/static/css/main.522022e3.chunk.css"
+  },
+  {
+    "revision": "72724b06355cea6e311b",
+    "url": "/static/js/2.ee0b8065.chunk.js"
+  },
+  {
+    "revision": "fb9e97254bd8dd2b0a6be4e5a7888783",
+    "url": "/static/js/2.ee0b8065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c812a429ee0b399dd8",
+    "url": "/static/js/main.fa29210d.chunk.js"
+  },
+  {
+    "revision": "8e055f39f1ae47ded08f",
+    "url": "/static/js/runtime-main.48902787.js"
+  },
+  {
+    "revision": "3e11d1ebcb2fd9212e3731ddd9042418",
+    "url": "/static/media/MASTER_LOGO.3e11d1eb.png"
+  },
+  {
+    "revision": "7a100f3b8c2ba479216e54e741b69dfe",
+    "url": "/static/media/Video.7a100f3b.mp4"
+  },
+  {
+    "revision": "807772217295163335e82c558db67cd0",
+    "url": "/static/media/athelete.80777221.jpg"
+  },
+  {
+    "revision": "dd1df8f478bb28433b5dfc6df4832f09",
+    "url": "/static/media/classFour.dd1df8f4.jpg"
+  },
+  {
+    "revision": "18ac1b5faa310265abb3815d3a79d0f4",
+    "url": "/static/media/classThree1.18ac1b5f.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "8f47b5b9abbd08324e6d2a00cb77bcb0",
+    "url": "/static/media/gymOverlayBg3.8f47b5b9.jpg"
+  },
+  {
+    "revision": "d794fb135928c626b641ad112a7b0e89",
+    "url": "/static/media/image1.d794fb13.jpeg"
+  },
+  {
+    "revision": "496cb0af7eed0a0174aa5cf3d1831f19",
+    "url": "/static/media/image10.496cb0af.jpeg"
+  },
+  {
+    "revision": "498051a99c642ce8c5ab98e83e60e1a1",
+    "url": "/static/media/image11.498051a9.jpeg"
+  },
+  {
+    "revision": "22ce1a3b490c8831977cd9233c0cc0ba",
+    "url": "/static/media/image2.22ce1a3b.jpeg"
+  },
+  {
+    "revision": "7789778cbdc2419138986d9f507e2c53",
+    "url": "/static/media/image3.7789778c.jpeg"
+  },
+  {
+    "revision": "1602d93a86a3d04a12b8528d36ad970c",
+    "url": "/static/media/image5.1602d93a.jpeg"
+  },
+  {
+    "revision": "c77a9ea5768b3719054c7444ad57f219",
+    "url": "/static/media/image6.c77a9ea5.jpeg"
+  },
+  {
+    "revision": "bec9a64abd7d660b7b1aab0c6cda7aa4",
+    "url": "/static/media/seminar.bec9a64a.jpg"
+  },
+  {
+    "revision": "8f36bf8b9f7094d39dc76b1184d794cb",
+    "url": "/static/media/thumbsUp.8f36bf8b.png"
+  },
+  {
+    "revision": "7c2ab25d46f7a6f96ce234d934f56a80",
+    "url": "/static/media/trainerOne.7c2ab25d.jpg"
+  },
+  {
+    "revision": "81e56a6dd10134e1affed0e5f247541f",
+    "url": "/static/media/trainerThree.81e56a6d.jpg"
+  },
+  {
+    "revision": "db6c16448e4272b3e4eb88cdc84c356f",
+    "url": "/static/media/trainerTwo.db6c1644.jpg"
+  },
+  {
+    "revision": "cfd33305db3ff6ac71f48366a3678e6b",
+    "url": "/static/media/vectoGames.cfd33305.jpg"
+  },
+  {
+    "revision": "b6c62282f340a854e44d038f90c1f399",
+    "url": "/static/media/vectoGen.b6c62282.jpg"
+  }
+]);
